9b1aea387a36c5a5c207c77c2d2ebd0a63d7e6fa
Sejal-g3 <sejalgheenah@gmail.com>
2024-10-21 12:37:17 +0400

    Display list of lessons functionality

---
+++ lessons.js
@@ -0,0 +1,194 @@
+let lessons = [
+    {
+        id: 1001,
+        subject: "Math",
+        location: "London",
+        price: 2000,
+        spaces: 5,
+        icon: "fa-solid fa-calculator"
+    },
+    {
+        id: 1002,
+        subject: "English",
+        location: "London",
+        price: 1000,
+        spaces: 5,
+        icon: "fa-solid fa-pen"
+    },
+    {
+        id: 1003,
+        subject: "Coding",
+        location: "London",
+        price: 2500,
+        spaces: 5,
+        icon: "fa-solid fa-laptop"
+    },
+    {
+        id: 1004,
+        subject: "Music",
+        location: "London",
+        price: 1500,
+        spaces: 5,
+        icon: "fa-solid fa-music"
+    },
+    {
+        id: 1005,
+        subject: "Art",
+        location: "London",
+        price: 1500,
+        spaces: 5,
+        icon: "fa-solid fa-palette"
+    },
+    {
+        id: 1006,
+        subject: "Dance",
+        location: "London",
+        price: 2.99,
+        spaces: 5,
+        icon: "fa-solid fa-image"
+    },
+    {
+        id: 1007,
+        subject: "Martial Arts",
+        location: "London",
+        price: 2.99,
+        spaces: 5,
+        icon: "fa-solid fa-image"
+    },
+    {
+        id: 1008,
+        subject: "Baking",
+        location: "London",
+        price: 2.99,
+        spaces: 5,
+        icon: "fa-solid fa-cake-candles"
+    },
+    {
+        id: 1009,
+        subject: "Sports",
+        location: "London",
+        price: 2.99,
+        spaces: 5,
+        icon: "fa-solid fa-medal"
+    },
+    {
+        id: 1010,
+        subject: "Fashion",
+        location: "London",
+        price: 2.99,
+        spaces: 5,
+        icon: "fa-solid fa-shirt"
+    },
+    {
+        id: 1011,
+        subject: "DIY Projects",
+        location: "London",
+        price: 2.99,
+        spaces: 5,
+        icon: "fa-solid fa-image"
+    },
+    {
+        id: 1012,
+        subject: "Drama",
+        location: "London",
+        price: 2.99,
+        spaces: 5,
+        icon: "fa-solid fa-image"
+    },
+    {
+        id: 2001,
+        subject: "Math",
+        location: "Mauritius",
+        price: 2000,
+        spaces: 5,
+        icon: "fa-solid fa-calculator"
+    },
+    {
+        id: 2002,
+        subject: "English",
+        location: "Mauritius",
+        price: 1000,
+        spaces: 5,
+        icon: "fa-solid fa-pen"
+    },
+    {
+        id: 2003,
+        subject: "Coding",
+        location: "Mauritius",
+        price: 2500,
+        spaces: 5,
+        icon: "fa-solid fa-laptop"
+    },
+    {
+        id: 2004,
+        subject: "Music",
+        location: "Mauritius",
+        price: 1500,
+        spaces: 5,
+        icon: "fa-solid fa-music"
+    },
+    {
+        id: 2005,
+        subject: "Art",
+        location: "Mauritius",
+        price: 1500,
+        spaces: 5,
+        icon: "fa-solid fa-palette"
+    },
+    {
+        id: 2006,
+        subject: "Dance",
+        location: "Mauritius",
+        price: 2.99,
+        spaces: 5,
+        icon: "fa-solid fa-image"
+    },
+    {
+        id: 2007,
+        subject: "Martial Arts",
+        location: "Mauritius",
+        price: 2.99,
+        spaces: 5,
+        icon: "fa-solid fa-image"
+    },
+    {
+        id: 2008,
+        subject: "Baking",
+        location: "Mauritius",
+        price: 2.99,
+        spaces: 5,
+        icon: "fa-solid fa-cake-candles"
+    },
+    {
+        id: 2009,
+        subject: "Sports",
+        location: "Mauritius",
+        price: 2.99,
+        spaces: 5,
+        icon: "fa-solid fa-medal"
+    },
+    {
+        id: 2010,
+        subject: "Fashion",
+        location: "Mauritius",
+        price: 2.99,
+        spaces: 5,
+        icon: "fa-solid fa-shirt"
+    },
+    {
+        id: 2011,
+        subject: "DIY Projects",
+        location: "Mauritius",
+        price: 2.99,
+        spaces: 5,
+        icon: "fa-solid fa-image"
+    },
+    {
+        id: 2012,
+        subject: "Drama",
+        location: "Mauritius",
+        price: 2.99,
+        spaces: 5,
+        icon: "fa-solid fa-image"
+    },
+]
